48f112a673ceef3f6251a68a1a55cac2737985bc
Антон Егоров <anton@192.168.1.10>
2022-07-11 18:08:11 +0300

    task6: odd

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int modulo = num % 2;
+if (modulo == 0)
+{
+    Console.WriteLine("Число " + num + " чётное");
+}
+else
+{
+    Console.WriteLine("Число " + num + " нечётное");
+}
