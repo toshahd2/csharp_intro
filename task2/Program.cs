e000def1aa943f788dd962569914c40fbde63992
Антон Егоров <anton@192.168.1.10>
2022-07-11 15:50:40 +0300

    task2: max

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите значение числа a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение числа b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+Console.WriteLine("a = " + a + ", b = " + b + ", max = " + max);
