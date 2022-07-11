5557159a05afe1cc5f615927f059f5bf9b643ddc
Антон Егоров <anton@192.168.1.10>
2022-07-11 16:01:18 +0300

    task4: 3 max

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение числа a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение числа b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение числа c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c + ", max = " + max);
