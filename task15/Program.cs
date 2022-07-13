45a2b859e92870b7cf5e546b1746ff5119a8c252
Антон Егоров <anton@192.168.1.4>
2022-07-13 19:16:06 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите цифру дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day > 0 && day < 6) Console.WriteLine("Будний день");
+else if(day > 5 && day < 8) Console.WriteLine("Выходной день");
+else Console.WriteLine("Нет такого дня недели");
