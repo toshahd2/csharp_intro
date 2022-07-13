fec37437112dd60b060c80724f874264653ef3fb
Антон Егоров <anton@192.168.1.4>
2022-07-13 19:03:57 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+if (num > 99)
+{
+    while (temp > 999)
+    {
+        temp = temp / 10;
+    }
+    int result = temp % 10;
+    Console.WriteLine("Третья цифра числа " + num + " - " + result);
+}
+else Console.WriteLine("Третьей цифры нет");
