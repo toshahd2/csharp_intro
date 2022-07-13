d2bd671253aa2a25d1b7038da9d1a559fb5502c9
Антон Егоров <anton@192.168.1.4>
2022-07-13 18:18:41 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+    int result = (num / 10) % 10;
+    Console.WriteLine("Вторая цифра числа " + num + " - " + result);
+}
+else Console.WriteLine("Вы ввели не трёхзначное число");
