cb4b7c60e86350073a215dd6d69084abc2a23a78
Антон Егоров <anton@192.168.1.15>
2022-07-25 13:48:52 +0300

    Extra2_3

---
+++ Extra2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите количество месяцев: ");
+int month = Convert.ToInt32(Console.ReadLine());
+double sum = 1000;
+for (int i = 0; i < (month); i++)
+{
+    sum = Math.Round(sum * 1.015, 2);
+    Console.Clear();
+    Console.WriteLine($"Баланс депозита по итогу {month} месяцев(-ца): {sum} рублей");
+}
