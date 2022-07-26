9c7d68e2e82b2f9a04aec670e9e566c1e34d02e4
Антон Егоров <anton@192.168.1.15>
2022-07-26 11:44:27 +0300

    Extra3_1

---
+++ Extra3_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+double y = 0;
+if (x > 0)
+{
+    y = Math.Pow(Math.Sin(x), 2);
+    Console.WriteLine("y = Math.Pow(Math.Sin(x), 2)");
+}
+else y = 1 - (2 * Math.Sin(Math.Pow(x, 2)));
+Console.WriteLine(y);
