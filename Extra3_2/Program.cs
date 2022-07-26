e015693f7ed3e1ace7a749bf773db09b445a3e0e
Антон Егоров <anton@192.168.1.15>
2022-07-26 13:56:55 +0300

    Extra3_2

---
+++ Extra3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано трёхзначное число N.
+// Определить кратна ли трём сумма всех его цифр.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num < 100 || num > 999)
+{
+    Console.Write("Вы ввели не трехзначное число, повторите ввод: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+int first = num / 100;
+int second = (num / 10) % 10;
+int third = num % 10;
+int sum = first + second + third;
+int modulo = sum % 3;
+if (modulo == 0)
+{
+    Console.WriteLine($"Сумма чисел равна {sum} и кратна трём");
+}
+else Console.WriteLine($"Сумма чисел равна {sum} и НЕ кратна трём");
