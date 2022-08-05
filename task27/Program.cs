74c01c0e85d41c089840c4018d89ba0c0f55508d
Антон Егоров <anton@192.168.1.15>
2022-08-05 18:58:28 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int temp = num;
+
+while (num > 0)
+{
+    sum = sum + (num % 10);
+    num = num / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {temp} = {sum}");
