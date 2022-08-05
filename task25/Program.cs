6b278dfeea7d18dc4540dfa536baab20c552ff4e
Антон Егоров <anton@192.168.1.15>
2022-08-05 18:26:52 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A, которое будет возводиться в степень: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B, которое будет степенью числа A: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+int result = num_a;
+
+for (int i = 2; i < (num_b + 1); i++)
+{
+    result = result * num_a;
+}
+
+Console.WriteLine ($"{num_a} в степени {num_b} будет равно {result}");
