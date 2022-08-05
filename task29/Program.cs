5e73f6d1a29b1eb9282e2c1d7d81ab0a6ca157cd
Антон Егоров <anton@192.168.1.15>
2022-08-05 20:54:59 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,36 @@
+﻿int size = 8;
+int[] array = new int[size];
+int temp;
+
+Console.Write("[ ");
+for (int i = 0; i < size; i++)
+{
+    Random rand = new Random();
+    temp = rand.Next(-9, 10);
+    array[i] = temp;
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("\b\b ]");
+Console.WriteLine();
+
+for (int j = size - 1; j > 0 ; j--)
+{
+    for (int l = 0; l < j; l++)
+    {
+        
+    
+        if (Math.Abs(array[l]) > Math.Abs(array[l+1]) )
+        {
+            int temp2 = array[l];
+            array[l] = array[l+1];
+            array[l+1] = temp2;
+        }
+    }
+}   
+
+Console.Write("[ ");
+for (int k = 0; k < size; k++)
+{
+    Console.Write($"{array[k]}, ");
+}
+Console.Write("\b\b ]");
