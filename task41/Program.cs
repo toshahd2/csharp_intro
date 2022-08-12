a4b44ab8d74a1669f5f19ca31dd141bbb7603594
Антон Егоров <anton@192.168.1.15>
2022-08-12 18:03:16 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿string dataInput;
+int size = 100;
+int[] array = new int[size];
+
+Console.Write("Введите числа, для остановки ввода напишите end: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    dataInput = Console.ReadLine();
+    if (dataInput != "end") array[i] = Convert.ToInt32(dataInput);
+    else break;
+}
+
+CountPositiveNumbers(array);
+
+void CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Console.WriteLine($"Вы ввели {count} чисел(числа) больше 0");
+}
