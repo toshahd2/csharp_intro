b11f2cc1e075c36387d7cfa9bebeaa2452ba5481
Антон Егоров <anton@192.168.1.15>
2022-08-14 17:10:02 +0300

    task43

---
+++ task47/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int rows = random.Next(3,10);
+int columns = random.Next(3,10);
+double[,] array = new double[rows, columns];
+FillArray(array, -100, 100);
+PrintArray(array);
+
+void FillArray (double[,] array, int startNumber = -50, int finishNumber = 50)
+{
+    Random rand = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(startNumber, finishNumber) + Math.Round(rand.NextDouble(), 1);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
