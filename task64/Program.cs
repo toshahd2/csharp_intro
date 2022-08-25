003492c68a5022782b94f4f84445e15353465fde
Антон Егоров <anton@Air-Anton.local>
2022-08-25 22:47:48 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Recursion (int m, int n)
+{
+    if(m > n) return "Все значения выведены";
+    if (m%3 == 0) Console.WriteLine(m);
+    m++;
+    return Recursion (m, n);
+}
+
+Console.WriteLine(Recursion(m, n));
