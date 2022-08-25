af8356a9752d4bb610d6cafd8d461309d4069851
Антон Егоров <anton@Air-Anton.local>
2022-08-25 23:09:13 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Recursion (int m, int n, int sum = 0)
+{
+    if(m > n) return sum;
+    sum += m;
+    m++;
+    return Recursion (m, n, sum);
+}
+
+Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n} равна {Recursion(m, n)}");
